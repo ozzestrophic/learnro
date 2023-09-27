432b2b32e9414fd5463371170cb84d8748410fe9
Ozzeek <osama.m.alnagar@gmail.com>
2023-09-27 18:17:06 +0300

    setup theme colors

---
+++ tailwind.config.js
@@ -0,0 +1,63 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    colors: {
+      'lrnro-blue': {
+        '50': '#e4f7ff',
+        '100': '#cfefff',
+        '200': '#a8dfff',
+        '300': '#74c7ff',
+        '400': '#3e9cff',
+        '500': '#1370ff',
+        '600': '#005eff',
+        '700': '#005eff',
+        '800': '#0054e4',
+        '900': '#003bb0',
+        '950': '#002780',
+        DEFAULT: '#002780', 
+    },
+      'lrnro-yellow': {
+        '50': '#fefde8',
+        '100': '#fffac2',
+        '200': '#fff489',
+        '300': '#ffe545',
+        '400': '#fcd20f',
+        '500': '#ecb906',
+        '600': '#cc9002',
+        '700': '#a26606',
+        '800': '#86500d',
+        '900': '#724111',
+        '950': '#432105',
+        DEFAULT: '#fcd20f',
+    },
+      'lrnro-red': {
+        '50': '#fff1f3',
+        '100': '#ffe0e4',
+        '200': '#ffc6cd',
+        '300': '#ff9ea9',
+        '400': '#ff6677',
+        '500': '#fc374d',
+        '600': '#ea1830',
+        '700': '#c81025',
+        '800': '#a31122',
+        '900': '#871522',
+        '950': '#4a050d',
+        DEFAULT: '#c81025', 
+    },
+      'lrnro-lightBlue': '#F2F6FF',
+      'lrnro-lightYellow': '#FFFAE5',
+      'lrnro-lightRed': '#FFD8DC',
+      
+  
+
+  
+    
+    },
+
+
+    extend: {},
+  },
+  plugins: [],
+}
+
